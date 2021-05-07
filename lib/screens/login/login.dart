c0849cf118374a15af19a74850661b5afd106d02
ShyrokyiOleksandr <shyrokyi.oleksandr@gmail.com>
2021-05-07 07:43:54 +0300

    Added files for login and sing up screens

---
+++ lib/screens/login/login.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+/// We use "Our-" prefix in order to not get confused with login of other
+/// services
+class OurLogin extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
